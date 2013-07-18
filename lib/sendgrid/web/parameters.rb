a8accf16ad967e75afb2d00124bfe4da85c320ec
Darren Coxall <darren.coxall@simplybusiness.co.uk>
2013-07-18 10:08:25 +0100

    Parameters will now be an extension of Hash
    This will mean that using it is as simple as using a standard
    Hash. It will automatically cleanse values at all points to prevent
    values being stored in incorrect formats. It also means it can be
    used in any situation that a Hash is used.

--- lib/sendgrid/web/parameters.rb
+++ lib/sendgrid/web/parameters.rb
@@ -1,40 +1,9 @@
-module Sendgrid::Web::Parameters
+class Sendgrid::Web::Parameters < Hash
 
-  refine Hash do
-
-    # Cleanse a Hash to prepare it for use with the SendGrid Web API.
-    # It converts objects that respond to #strftime into the
-    # +YYYY-MM-DD+ format.
-    #
-    # @return [Hash] +self+ after modifying the stored data.
-    # @note Fallback is to call #to_s which means numeric values aren't
-    #   correctly comparable afterwards.
-    def cleanse!
-      self.each do |key, value|
-        if value.respond_to?(:strftime)
-          value = value.strftime("%Y-%m-%d")
-        elsif value.is_a?(Hash)
-          value.cleanse!
-        else
-          value = value.to_s
-        end
-        self[key] = value
-      end
-      self
-    end
-
-    # Cleanse a Hash to prepare it for use with the SendGrid Web API.
-    # It converts objects that respond to #strftime into the
-    # +YYYY-MM-DD+ format.
-    #
-    # @return [Hash] a new Hash with the modified values.
-    # @note Fallback is to call #to_s which means numeric values aren't
-    #   correctly comparable afterwards.
-    # @see #cleanse!
-    def cleanse
-      dup.cleanse!
-    end
-
+  def []=(key, value)
+    value = value.strftime('%Y-%m-%d') if value.respond_to?(:strftime)
+    value = value.to_s
+    super
   end
 
 end
--- spec/sendgrid/web/parameters_spec.rb
+++ spec/sendgrid/web/parameters_spec.rb
@@ -1,57 +1,36 @@
 require 'spec_helper'
 
-using Sendgrid::Web::Parameters
-
 describe Sendgrid::Web::Parameters do
 
-  describe '#cleanse!' do
-
-    describe 'handles objects that respond to #strftime' do
-      context DateTime do
-        it 'converts values to YYYY-MM-DD' do
-          hash = { provider: DateTime.new(2013, 01, 23) }
-          hash.cleanse!
-          hash[:provider].should eql('2013-01-23')
-        end
-      end
-      context Time do
-        it 'converts values to YYYY-MM-DD' do
-          hash = { provider: Time.new(2013, 01, 23) }
-          hash.cleanse!
-          hash[:provider].should eql('2013-01-23')
-        end
-      end
-    end
-
-    describe 'handles booleans' do
-      it 'converts values to "true"' do
-        hash = { provider: true }
-        hash.cleanse!
-        hash[:provider].should eql('true')
-      end
-      it 'converts values to "false"' do
-        hash = { provider: false }
-        hash.cleanse!
-        hash[:provider].should eql('false')
-      end
-    end
-
-    it 'is recursive' do
-      hash = { provider: { date: DateTime.new(2013, 01, 23) } }
-      hash.cleanse!
-      hash[:provider][:date].should eql('2013-01-23')
-    end
-
+  describe 'is based on Hash' do
+    subject { described_class }
+    its(:superclass) { should eql(Hash) }
   end
 
-  describe '#cleanse' do
-    it 'creates a new Hash instance' do
-      hash = Hash.new
-      dup_hash = double(:dup_hash)
-      dup_hash.should_receive(:cleanse!)
-      hash.should_receive(:dup).
-        and_return(dup_hash)
-      hash.cleanse
+  describe 'cleanses when setting values' do
+    context DateTime do
+      before { subject[:provider] = DateTime.new(2013, 01, 23) }
+      it 'converts to YYYY-MM-DD format' do
+        subject[:provider].should eql('2013-01-23')
+      end
+    end
+    context Time do
+      before { subject[:provider] = Time.new(2013, 01, 23) }
+      it 'converts to YYYY-MM-DD format' do
+        subject[:provider].should eql('2013-01-23')
+      end
+    end
+    context true do
+      before { subject[:provider] = true }
+      it 'converts to "true"' do
+        subject[:provider].should eql('true')
+      end
+    end
+    context false do
+      before { subject[:provider] = false }
+      it 'converts to "false"' do
+        subject[:provider].should eql('false')
+      end
     end
   end
 
