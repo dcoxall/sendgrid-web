74808f16c5a7641156ffbd6f2ea7b84fa92ae3f7
Darren Coxall <darren@darrencoxall.com>
2013-07-20 08:47:55 +0100

    Finished Parameters class.
    Parameters act identically to a Hash except they format input to
    prepare it for use in the Sendgrid API.

--- lib/sendgrid/web/parameters.rb
+++ lib/sendgrid/web/parameters.rb
@@ -1,9 +1,50 @@
 class Sendgrid::Web::Parameters < Hash
 
   def []=(key, value)
-    value = value.strftime('%Y-%m-%d') if value.respond_to?(:strftime)
-    value = value.to_s
-    super
+    super(key, _cleanse_value(value))
+  end
+
+  def merge!(hash = {})
+    super(_cleanse(hash))
+  end
+
+  def merge(hash = {})
+    super(_cleanse(hash))
+  end
+
+  def replace(hash = {})
+    super(_cleanse(hash))
+  end
+
+  def store(key, value)
+    super(key, _cleanse_value(value))
+  end
+
+  def update(hash = {})
+    super(_cleanse(hash))
+  end
+
+  private
+
+  def _cleanse(hash)
+    cleansed = Hash.new
+    hash.each do |k, v|
+      cleansed[k] = _cleanse_value(v)
+    end
+    cleansed
+  end
+
+  def _cleanse_value(value)
+    if value.is_a?(Hash)
+      value = _cleanse(value)
+    elsif value.is_a?(Array)
+      value = value.collect { |v| _cleanse_value(v) }
+    elsif value.respond_to?(:strftime)
+      value = value.strftime('%Y-%m-%d')
+    else
+      value = value.to_s
+    end
+    value
   end
 
 end
--- spec/sendgrid/web/parameters_spec.rb
+++ spec/sendgrid/web/parameters_spec.rb
@@ -34,4 +34,118 @@ describe Sendgrid::Web::Parameters do
     end
   end
 
+  describe 'cleanses when merging' do
+    context DateTime do
+      before { subject.merge!(provider: DateTime.new(2013, 01, 23)) }
+      it 'converts to YYYY-MM-DD format' do
+        subject[:provider].should eql('2013-01-23')
+      end
+    end
+    context Time do
+      before { subject.merge!(provider: Time.new(2013, 01, 23)) }
+      it 'converts to YYYY-MM-DD format' do
+        subject[:provider].should eql('2013-01-23')
+      end
+    end
+    context true do
+      before { subject.merge!(provider: true) }
+      it 'converts to "true"' do
+        subject[:provider].should eql('true')
+      end
+    end
+    context false do
+      before { subject.merge!(provider: false) }
+      it 'converts to "false"' do
+        subject[:provider].should eql('false')
+      end
+    end
+    it 'into new Parameters' do
+      params = subject.merge(provider: true)
+      params.should be_instance_of(Sendgrid::Web::Parameters)
+      params[:provider].should eql('true')
+      params.object_id.should_not eql(subject.object_id)
+    end
+  end
+
+  describe 'cleanses when replacing' do
+    context DateTime do
+      before { subject.replace(provider: DateTime.new(2013, 01, 23)) }
+      it 'converts to YYYY-MM-DD format' do
+        subject[:provider].should eql('2013-01-23')
+      end
+    end
+    context Time do
+      before { subject.replace(provider: Time.new(2013, 01, 23)) }
+      it 'converts to YYYY-MM-DD format' do
+        subject[:provider].should eql('2013-01-23')
+      end
+    end
+    context true do
+      before { subject.replace(provider: true) }
+      it 'converts to "true"' do
+        subject[:provider].should eql('true')
+      end
+    end
+    context false do
+      before { subject.replace(provider: false) }
+      it 'converts to "false"' do
+        subject[:provider].should eql('false')
+      end
+    end
+  end
+
+  describe 'cleanses when storing' do
+    context DateTime do
+      before { subject.store(:provider, DateTime.new(2013, 01, 23)) }
+      it 'converts to YYYY-MM-DD format' do
+        subject[:provider].should eql('2013-01-23')
+      end
+    end
+    context Time do
+      before { subject.store(:provider, Time.new(2013, 01, 23)) }
+      it 'converts to YYYY-MM-DD format' do
+        subject[:provider].should eql('2013-01-23')
+      end
+    end
+    context true do
+      before { subject.store(:provider, true) }
+      it 'converts to "true"' do
+        subject[:provider].should eql('true')
+      end
+    end
+    context false do
+      before { subject.store(:provider, false) }
+      it 'converts to "false"' do
+        subject[:provider].should eql('false')
+      end
+    end
+  end
+
+  describe 'cleanses when updating' do
+    context DateTime do
+      before { subject.update(provider: DateTime.new(2013, 01, 23)) }
+      it 'converts to YYYY-MM-DD format' do
+        subject[:provider].should eql('2013-01-23')
+      end
+    end
+    context Time do
+      before { subject.update(provider: Time.new(2013, 01, 23)) }
+      it 'converts to YYYY-MM-DD format' do
+        subject[:provider].should eql('2013-01-23')
+      end
+    end
+    context true do
+      before { subject.update(provider: true) }
+      it 'converts to "true"' do
+        subject[:provider].should eql('true')
+      end
+    end
+    context false do
+      before { subject.update(provider: false) }
+      it 'converts to "false"' do
+        subject[:provider].should eql('false')
+      end
+    end
+  end
+
 end
